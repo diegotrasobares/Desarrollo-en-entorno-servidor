61aeaa0709893e0cc11f9fff3b4ac2bdc4d956e5
Diego <trasobares.diego.tpr3a@gmail.com>
2022-12-17 19:48:02 +0100

    Creado acceso con PDO

--- accesoConPDO/app/models/AccesoDatos.php
+++ accesoConPDO/app/models/AccesoDatos.php
@@ -1,5 +1,6 @@
 <?php
-
+include_once "Cliente.php";
+include_once "app/config/configDB.php";
 /*
  * Acceso a datos con BD Usuarios : 
  * Usando la librería mysqli
@@ -10,6 +11,11 @@ class AccesoDatos {
     
     private static $modelo = null;
     private $dbh = null;
+    private $stmt_usuarios = null;
+    private $stmt_usuario  = null;
+    private $stmt_boruser  = null;
+    private $stmt_moduser  = null;
+    private $stmt_creauser = null;
     
     public static function getModelo(){
         if (self::$modelo == null){
@@ -24,21 +30,34 @@ class AccesoDatos {
    
     private function __construct(){
         
-       
-         $this->dbh = new mysqli(DB_SERVER,DB_USER,DB_PASSWD,DATABASE);
-         
-      if ( $this->dbh->connect_error){
-         die(" Error en la conexión ".$this->dbh->connect_errno);
-        } 
-
+        try {
+            $dsn = "mysql:host=".DB_SERVER.";dbname=".DATABASE.";charset=utf8";
+            $this->dbh = new PDO($dsn,DB_USER,DB_PASSWD);
+            $this->dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e){
+            echo "Error de conexión ".$e->getMessage();
+            exit();
+        }
+         // Construyo las consultas
+     $this->stmt_usuarios  = $this->dbh->prepare("select * from Clientes LIMIT :primero,:cuantos");
+     $this->stmt_usuario   = $this->dbh->prepare("select * from Clientes where id=:id");
+     $this->stmt_boruser   = $this->dbh->prepare("delete from Clientes where id =:id");
+     $this->stmt_moduser   = $this->dbh->prepare("UPDATE Clientes set first_name=:nombre,id=:id,last_name=:lastname,email=:email,"."gender=:gender, ip_address=:ip,telefono=:telefono WHERE id=:id");
+     $this->stmt_creauser  = $this->dbh->prepare("INSERT INTO `Clientes`( `first_name`, `last_name`, `email`, `gender`, `ip_address`, `telefono`)"."Values(?,?,?,?,?,?)");
+    $this->stmt_numUsuarios =$this->dbh->prepare("select id from Clientes");
     }
 
     // Cierro la conexión anulando todos los objectos relacioanado con la conexión PDO (stmt)
     public static function closeModelo(){
         if (self::$modelo != null){
             $obj = self::$modelo;
-            // Cierro la base de datos
-            $obj->dbh->close();
+            $obj->stmt_usuarios = null;
+            $obj->stmt_usuario  = null;
+            $obj->stmt_boruser  = null;
+            $obj->stmt_moduser  = null;
+            $obj->stmt_creauser = null;
+            $obj->stmt_numUsuarios=null;
+            $obj->dbh = null;
             self::$modelo = null; // Borro el objeto.
         }
     }
@@ -47,8 +66,12 @@ class AccesoDatos {
     // Devuelvo cuantos filas tiene la tabla
 
     public function numClientes ():int {
-      $result = $this->dbh->query("SELECT id FROM Clientes");
-      $num = $result->num_rows;  
+
+        if ( $this->stmt_numUsuarios->execute() ){
+            $num = $this->stmt_numUsuarios->rowCount();
+        }
+
+
       return $num;
     } 
     
@@ -56,49 +79,37 @@ class AccesoDatos {
     // SELECT Devuelvo la lista de Usuarios
     public function getClientes ($primero,$cuantos):array {
         $tuser = [];
-        // Crea la sentencia preparada
-       // echo "<h1> $primero : $cuantos  </h1>";
-        $stmt_usuarios  = $this->dbh->prepare("select * from Clientes limit $primero,$cuantos");
-        // Si falla termina el programa
-        if ( $stmt_usuarios == false) die (__FILE__.':'.__LINE__.$this->dbh->error);
-        // Ejecuto la sentencia
-        $stmt_usuarios->execute();
-        // Obtengo los resultados
-        $result = $stmt_usuarios->get_result();
-        // Si hay resultado correctos
-        if ( $result ){
-            // Obtengo cada fila de la respuesta como un objeto de tipo Usuario
-            while ( $user = $result->fetch_object('Cliente') ){
+        $this->stmt_usuarios->bindValue(':primero',(int) $primero,PDO::PARAM_INT);
+        $this->stmt_usuarios->bindValue(':cuantos', (int) $cuantos,PDO::PARAM_INT);
+        $this->stmt_usuarios->setFetchMode(PDO::FETCH_CLASS, 'cliente');
+        if ( $this->stmt_usuarios->execute() ){
+            while ( $user = $this->stmt_usuarios->fetch()){
                $tuser[]= $user;
             }
         }
-        // Devuelvo el array de objetos
         return $tuser;
+
+
     }
     
       
     // SELECT Devuelvo un usuario o false
     public function getCliente (int $id) {
-        $cli = false;
+        $user = false;
         
-        $stmt_usuario   = $this->dbh->prepare("select * from Clientes where id =?");
-        if ( $stmt_usuario == false) die ($this->dbh->error);
-
-        // Enlazo $login con el primer ? 
-        $stmt_usuario->bind_param("i",$id);
-        $stmt_usuario->execute();
-        $result = $stmt_usuario->get_result();
-        if ( $result ){
-            $cli = $result->fetch_object('Cliente');
+        $this->stmt_usuario->setFetchMode(PDO::FETCH_CLASS, 'cliente');
+        $this->stmt_usuario->bindParam(':id', $id);
+        if ( $this->stmt_usuario->execute() ){
+             if ( $obj = $this->stmt_usuario->fetch()){
+                $user= $obj;
             }
-        
-        return $cli;
+        }
+        return $user;
     }
      
     public function getClienteSiguiente($id){
 
         $cli = false;
-        
         $stmt_usuario   = $this->dbh->prepare("select * from Clientes where id >? limit 1");
         if ( $stmt_usuario == false) die ($this->dbh->error);
 
@@ -139,43 +150,32 @@ class AccesoDatos {
     // UPDATE TODO
     public function modCliente($cli):bool{
       
-        $stmt_moduser   = $this->dbh->prepare("UPDATE Clientes set first_name=?,last_name=?,email=?,".
-         "gender=?, ip_address=?,telefono=? WHERE id=?");
-        if ( $stmt_moduser == false) die ($this->dbh->error);
-
-        $stmt_moduser->bind_param("ssssssi",$cli->first_name,$cli->last_name,$cli->email,
-                    $cli->gender,$cli->ip_address,$cli->telefono,$cli->id);
-        $stmt_moduser->execute();
-        $resu = ($this->dbh->affected_rows  == 1);
+        $this->stmt_moduser->bindValue(':nombre',$cli->first_name);
+        $this->stmt_moduser->bindValue(':lastname',$cli->last_name);
+        $this->stmt_moduser->bindValue(':id',$cli->id);
+        $this->stmt_moduser->bindValue(':email',$cli->email);
+        $this->stmt_moduser->bindValue(':gender',$cli->gender);
+        $this->stmt_moduser->bindValue(':ip',$cli->ip_address);
+        $this->stmt_moduser->bindValue(':telefono',$cli->telefono);
+        $this->stmt_moduser->execute();
+        $resu = ($this->stmt_moduser->rowCount () == 1);
         return $resu;
     }
 
   
     //INSERT 
     public function addCliente($cli):bool{
-       
-        // El id se define automáticamente por autoincremento.
-        $stmt_creauser  = $this->dbh->prepare(
-            "INSERT INTO `Clientes`( `first_name`, `last_name`, `email`, `gender`, `ip_address`, `telefono`)".
-            "Values(?,?,?,?,?,?)");
-        if ( $stmt_creauser == false) die ($this->dbh->error);
-
-        $stmt_creauser->bind_param("ssssss",$cli->first_name,$cli->last_name,$cli->email,
-        $cli->gender,$cli->ip_address,$cli->telefono);
-        $stmt_creauser->execute();
-        $resu = ($this->dbh->affected_rows  == 1);
+        $this->stmt_creauser->execute( [$cli->first_name,$cli->last_name,$cli->email,$cli->gender,$cli->ip_address,$cli->telefono]);
+        $resu = ($this->stmt_creauser->rowCount () == 1);
         return $resu;
     }
 
    
     //DELETE 
     public function borrarCliente(int $id):bool {
-        $stmt_boruser   = $this->dbh->prepare("delete from Clientes where id =?");
-        if ( $stmt_boruser == false) die ($this->dbh->error);
-       
-        $stmt_boruser->bind_param("i", $id);
-        $stmt_boruser->execute();
-        $resu = ($this->dbh->affected_rows  == 1);
+        $this->stmt_boruser->bindValue(':id', $id);
+        $this->stmt_boruser->execute();
+        $resu = ($this->stmt_boruser->rowCount () == 1);
         return $resu;
     }   
     
